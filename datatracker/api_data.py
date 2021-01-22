ddc9018236789eab4e86124a34e131dba6765512
phher802 <your_email@example.com>
2021-01-22 00:20:04 -0600

    Create api data file with api call and json encoding

---
+++ datatracker/api_data.py
@@ -0,0 +1,15 @@
+import requests
+import json
+from types import SimpleNamespace
+
+response = requests.get('https://api.dccresource.com/api/games')
+games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+
+with open('games.txt', "w") as json_file:
+    json.dump(games, json_file, indent=4, sort_keys=True)
+
+
+
+
+
+
---
+++ datatracker/main.py
@@ -0,0 +1,7 @@
+import api_data
+import get_lists
+
+if __name__ == "main":
+    game_details = api_data()
+
+
