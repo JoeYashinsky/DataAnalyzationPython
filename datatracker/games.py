1d06b6d7f1b8c6c8c74133b3ba592ad12b6dd8cf
Zachary Inkster <z.j.inkster@gmail.com>
2021-01-24 11:06:41 -0600

    Made new view and created method to populate list of games by name.

--- datatracker/games.py
+++ datatracker/games.py
@@ -20,3 +20,13 @@ def get_global_sales():
                 list_games.append(game)
     return render_template('our_views/init.html', list_games=list_games, response=response)
 
+
+@bp.route('/namedgames', methods=['GET'])
+def get_games_by_name():
+    game_names = []
+    response = requests.get('https://api.dccresource.com/api/games')
+    games = response.json()
+    for game in games:
+        game_name = game['name']
+        game_names.append(game_name)
+    return render_template('our_views/namedgames.html', game_names=game_names, response=response)
