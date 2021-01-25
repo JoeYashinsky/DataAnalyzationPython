458760be656a7e1218f62761a48bc801018b22a2
phher802 <your_email@example.com>
2021-01-24 23:22:34 -0600

    Edit global_sales function to get platform list and global sales list

--- datatracker/games.py
+++ datatracker/games.py
@@ -25,6 +25,7 @@ def get_games():
 @bp.route('/globalSales', methods=['GET'])
 def global_sales():
     list_games = []
+    list_platforms = []
     response = requests.get('https://api.dccresource.com/api/games')
     games = response.json()
 
@@ -34,10 +35,17 @@ def global_sales():
             if game_year >= 2013:
                 list_games.append(game)
 
-    platform_values = [sub['platform'] for sub in list_games]
+    #platform_values = [sub['platform'] for sub in list_games]
+    for game in list_games:
+        platform = game['platform']
+        if platform is not None:
+            if platform not in list_platforms:
+                list_platforms.append(platform)
+
+
     global_values = [sub['globalSales'] for sub in list_games]
 
-    return render_template('our_views/globalSales.html', list_games=list_games, platform_values=platform_values, global_values=global_values, response=response)
+    return render_template('our_views/globalSales.html', list_games=list_games, list_platforms=list_platforms, global_values=global_values, response=response)
 
 
 @bp.route('/namedgames', methods=['GET'])
@@ -52,3 +60,17 @@ def get_games_by_name():
             game_names.append(game_name)
 
     return render_template('our_views/namedgames.html', game_names=game_names, response=response)
+
+
+@bp.route('/init', methods=['GET'])
+def copies_sold():
+    response = requests.get('https://api.dccresource.com/api/games')
+    games = response.json()['items']
+
+    for game in games:
+        return game['platform']
+
+    copies = copies_sold()
+    games_sold = [copy for copy in copies if copy['platform'] is not None and copy['globalSales'] is not None]
+
+    return render_template('our_views/init.html', response=response)
