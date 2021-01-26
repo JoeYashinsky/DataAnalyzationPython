07a3f11e3da8223763b2827adf5b3049d2bc9e02
Zachary Inkster <z.j.inkster@gmail.com>
2021-01-26 11:36:04 -0600

    Created view and function for NA Sales and started styling the views.

--- datatracker/games.py
+++ datatracker/games.py
@@ -52,10 +52,37 @@ def global_sales():
                            global_values=global_values, response=response)
 
 
+# How many copies of each game per console were sold in North America?
+
+
+@bp.route('/naSales', methods=(['GET']))
+def na_sales_games():
+    list_games = []
+    list_platforms = []
+    response = requests.get('https://api.dccresource.com/api/games')
+    games = response.json()
+
+    for game in games:
+        platform = game['platform']
+        if platform is not None:
+            if platform not in list_platforms:
+                list_platforms.append(platform)
+
+    global_values = dict.fromkeys(list_platforms, 0)
+    for game in games:
+        for item in global_values:
+            if game['platform'] == item:
+                global_values[item] += game['naSales']
+
+    return render_template('our_views/naSales.html', list_games=list_games, list_platforms=list_platforms,
+                           global_values=global_values, response=response)
+
+
 @bp.route('/namedGames', methods=('GET', 'POST'))
 def search_for_game():
     response = requests.get('https://api.dccresource.com/api/games')
     games = response.json()
+    list_platforms = []
     found_game = []
     searched_game = ""
     if request.method == 'POST':
@@ -67,8 +94,27 @@ def search_for_game():
             if game['name'] == searched_game:
                 found_game.append(game)
 
-    return render_template('our_views/namedGames.html', found_game=found_game, searched_game=searched_game,
-                           response=response)
+    # display chart repping #of copies sold per platform
+    # iterate through games
+    # if game[name] == searched_game
+    # iterate through global_values
+    # global_values[key] += game[globalSales]
+
+    for game in games:
+        platform = game['platform']
+        if platform is not None:
+            if platform not in list_platforms:
+                list_platforms.append(platform)
+
+    global_values = dict.fromkeys(list_platforms, 0)
+    for game in games:
+        # if game['name'] == searched_game:
+        for item in global_values:
+            if game['platform'] == item and game['name'] == searched_game:
+                global_values[item] += game['globalSales']
+
+    return render_template('our_views/namedGames.html', global_values=global_values, found_game=found_game,
+                           searched_game=searched_game, list_platforms=list_platforms, response=response)
 
 
 @bp.route('/init', methods=['GET'])
